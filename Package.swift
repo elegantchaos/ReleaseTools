fe48a48eb0fb236cae2b7336139b3d3c7c59cc80
Sam Deane <sam@elegantchaos.com>
2019-06-19 12:15:55 +0100

    require 10.13

--- Package.swift
+++ Package.swift
@@ -1,16 +1,19 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ReleaseTools",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     products: [
         .executable(name: "rt", targets: ["ReleaseTools"]),
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.2"),
-        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.0.4"),
+        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
