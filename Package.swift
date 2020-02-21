7e69a541ea0424d5c7947e21ed23c49226594f16
Sam Deane <sam@elegantchaos.com>
2020-02-21 18:34:47 +0000

    Updated to newer packages

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .executable(name: "rt", targets: ["ReleaseTools"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.3"),
-        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.1.0"),
+        .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.5"),
+        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
