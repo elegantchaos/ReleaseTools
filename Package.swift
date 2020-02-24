997dea7c35a9334f842e0b9665b7b3e7b006708b
Sam Deane <sam@elegantchaos.com>
2020-02-24 19:35:27 +0000

    Fixed tests

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.5"),
         .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.1.3"),
+        .package(url: "https://github.com/elegantchaos/XCTestExtensions.git", from: "1.0.9"),
     ],
     targets: [
         .target(
@@ -20,6 +21,6 @@ let package = Package(
             dependencies: ["CommandShell", "Runner"]),
         .testTarget(
             name: "ReleaseToolsTests",
-            dependencies: ["ReleaseTools"]),
+            dependencies: ["ReleaseTools", "XCTestExtensions"]),
     ]
 )
--- Tests/ReleaseToolsTests/ReleaseToolsTests.swift
+++ Tests/ReleaseToolsTests/ReleaseToolsTests.swift
@@ -1,47 +1,13 @@
 import XCTest
-import class Foundation.Bundle
+import XCTestExtensions
+import Runner
 
 final class ReleaseToolTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        let fooBinary = productsDirectory.appendingPathComponent("ReleaseTools")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
+    func testNoArguments() throws {
+        let rt = Runner(for: productsDirectory.appendingPathComponent("rt"))
+        let result = try! rt.sync(arguments: [])
+        XCTAssertEqual(result.status, 2)
+        XCTAssertTrue(result.stdout.contains("Usage:\n"))
     }
 
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
