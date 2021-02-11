b942622ca8478d5d83fa348d92c4a25baa6a5c0c
Sam Deane <sam@elegantchaos.com>
2021-02-11 18:06:52 +0000

    Bumped metadata.

--- Sources/ReleaseTools/meta.swift
+++ Sources/ReleaseTools/meta.swift
@@ -1,8 +1,8 @@
     struct Metadata {
-        let build = "124"
-        let version = "v1.1.1-26-g40d2e6e"
-        let commit = "40d2e6e0044f4ce8348d5a4f426cecdb32d61bc3"
-        let tags = "heads/master"
+        let build = "155"
+        let version = "v1.2.0-11-g8f6fea6"
+        let commit = "8f6fea6482c4f54bc05fdcc6591d2ec2bff1f54c"
+        let tags = "heads/development"
 
         static var main = Metadata()
     }
