e7e159fc308ace0e85996fa95270cfb91dd7de98
Sam Deane <sam@elegantchaos.com>
2021-02-12 10:55:25 +0000

    Bumped version, added some todo notes.

--- Sources/ReleaseTools/meta.swift
+++ Sources/ReleaseTools/meta.swift
@@ -1,8 +1,8 @@
     struct Metadata {
-        let build = "159"
-        let version = "v1.3.0"
-        let commit = "4bdb291ff5013a3da3e94f3f393eb6cdc6f133ce"
-        let tags = "tags/v1.3.0"
+        let build = "160"
+        let version = "v1.3.0-1-g91d9172"
+        let commit = "91d9172e63356ff97187e045c0901045be631451"
+        let tags = "heads/master"
 
         static var main = Metadata()
     }
