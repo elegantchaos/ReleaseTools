af401e07b22e1e028908e5ab2f3e995a6497e2ba
Sam Deane <sam@elegantchaos.com>
2023-05-15 15:21:41 +0100

    Updated metadata

--- Sources/ReleaseTools/meta.swift
+++ Sources/ReleaseTools/meta.swift
@@ -1,8 +1,8 @@
     struct Metadata {
-        let build = "173"
-        let version = "v1.3.4"
-        let commit = "7420bf15185443485d9a7e9ff9596d1e8c41474a"
-        let tags = "tags/v1.3.4"
+        let build = "182"
+        let version = "v1.3.6"
+        let commit = "3a2ca6fcb7e926f704f136cc0b6f699bcdf1d28a"
+        let tags = "tags/v1.3.6"
 
         static var main = Metadata()
     }
