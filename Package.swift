d74cab5ae6f6f690e0d8135844100a772786a6d0
Sam Deane <sam@elegantchaos.com>
2024-09-03 17:31:19 +0100

    Fixed mac build

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/elegantchaos/Coercion.git", from: "1.1.2"),
     .package(url: "https://github.com/elegantchaos/Files.git", from: "1.2.0"),
-    .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.6.0"),
+    .package(url: "https://github.com/elegantchaos/Logger.git", branch: "v2-wip"),
     .package(url: "https://github.com/elegantchaos/Runner.git", from: "2.0.5"),
     .package(url: "https://github.com/elegantchaos/ChaosByteStreams", from: "1.0.0"),
     .package(url: "https://github.com/elegantchaos/Versionator.git", from: "2.0.2"),
