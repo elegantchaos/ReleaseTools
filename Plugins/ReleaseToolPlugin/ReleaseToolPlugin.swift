3ac420481fbb8b4e22fb8589c974c6080a9c5d95
Sam Deane <sam@elegantchaos.com>
2024-09-03 16:46:16 +0100

    Added sync xcode version

--- Plugins/ReleaseToolPlugin/ReleaseToolPlugin.swift
+++ Plugins/ReleaseToolPlugin/ReleaseToolPlugin.swift
@@ -7,50 +7,71 @@ import Foundation
 import PackagePlugin
 
 @main struct ReleaseToolsPlugin: CommandPlugin {
-  func run(tool: String, arguments: [String], context: PackagePlugin.PluginContext, cwd: URL) async throws -> String {
-    let tool = try context.tool(named: tool)
-
-    Diagnostics.remark("Running \(tool) \(arguments.joined(separator: " ")).")
-
-    let outputPipe = Pipe()
-    return try await withCheckedThrowingContinuation { continuation in
-      let process = Process()
-      process.executableURL = tool.url
-      process.arguments = arguments
-      process.currentDirectoryURL = cwd
-
-      process.standardOutput = outputPipe
-      process.terminationHandler = { process in
-        let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        continuation.resume(
-          returning: String(decoding: data, as: UTF8.self)
-        )
-      }
-      do {
-        try process.run()
-      } catch {
-        continuation.resume(throwing: error)
-      }
+    
+    func run(tool: PluginContext.Tool, arguments: [String], cwd: URL) async throws -> String {
+        Diagnostics.remark("Running \(tool) \(arguments.joined(separator: " ")).")
+        
+        let outputPipe = Pipe()
+        return try await withCheckedThrowingContinuation { continuation in
+            let process = Process()
+            process.executableURL = tool.url
+            process.arguments = arguments
+            process.currentDirectoryURL = cwd
+            
+            process.standardOutput = outputPipe
+            process.terminationHandler = { process in
+                let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+                continuation.resume(
+                    returning: String(decoding: data, as: UTF8.self)
+                )
+            }
+            do {
+                try process.run()
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        }
     }
-  }
-
-  func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
-    let output = try await run(
-      tool: "ActionBuilderTool", arguments: arguments, context: context, cwd: context.package.directoryURL)
-
-    Diagnostics.remark(output)
-  }
+    
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        let tool = try context.tool(
+            named: "ActionBuilderTool"
+        )
+        let output = try await run(
+            tool: tool, arguments: arguments, cwd: context.package.directoryURL)
+        
+        Diagnostics.remark(output)
+    }
 }
 
 #if canImport(XcodeProjectPlugin)
-  import XcodeProjectPlugin
+import XcodeProjectPlugin
 
-  extension ReleaseToolsPlugin: XcodeCommandPlugin {
+func runSync(tool: PluginContext.Tool, arguments: [String], cwd: URL) throws -> String {
+    Diagnostics.remark("Running \(tool) \(arguments.joined(separator: " ")).")
+    
+    let process = Process()
+    process.executableURL = tool.url
+    process.arguments = arguments
+    process.currentDirectoryURL = cwd
+    
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+    try process.run()
+    process.waitUntilExit()
+    
+    let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    return String(decoding: data, as: UTF8.self)
+}
+
+extension ReleaseToolsPlugin: XcodeCommandPlugin {
     func performCommand(context: XcodePluginContext, arguments: [String]) throws {
-      let output = try await run(
-        tool: "ActionBuilderTool", arguments: arguments, context: context, cwd: context.package.directoryURL)
-
-      Diagnostics.remark(output)
+        let tool = try context.tool(
+            named: "ActionBuilderTool"
+        )
+        
+        let output = try runSync(tool: tool, arguments: arguments, cwd: context.xcodeProject.directoryURL)
+        Diagnostics.remark(output)
     }
-  }
+}
 #endif
