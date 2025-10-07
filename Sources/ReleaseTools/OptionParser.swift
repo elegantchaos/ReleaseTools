38dcc27d8c00710ce60c29a4385b0d87a615394f
Sam Deane <sam@elegantchaos.com>
2025-10-07 17:36:14 +0100

    tweaked path

--- Sources/ReleaseTools/OptionParser.swift
+++ Sources/ReleaseTools/OptionParser.swift
@@ -94,7 +94,7 @@ class OptionParser {
   var buildURL: URL { return rootURL.appendingPathComponents([".build", platform]) }
   var archiveURL: URL { return buildURL.appendingPathComponent("archive.xcarchive") }
   var exportURL: URL { return buildURL.appendingPathComponent("export") }
-  var uploadURL: URL { return exportURL.appendingPathComponent("upload") }
+  var uploadURL: URL { return buildURL.appendingPathComponent("upload") }
   var stapledURL: URL { return buildURL.appendingPathComponent("stapled") }
   var versionTag: String { return "v\(archive.version)-\(archive.build)-\(platform)" }
 
